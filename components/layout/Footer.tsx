82945e54e28d4d9931dd45d6f9d986aa8e307dae
Amarire Dev <108834223+Said-Amarire@users.noreply.github.com>
2025-12-06 11:52:31 +0100

    Update Footer.tsx

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -1,1 +1,12 @@
+import React from "react";
+
+const Footer: React.FC = () => {
+  return (
+    <footer className="bg-gray-100 p-6 mt-10 text-center">
+      <p>&copy; 2025 MyProperties. All rights reserved.</p>
+    </footer>
+  );
+};
+
+export default Footer;
 
